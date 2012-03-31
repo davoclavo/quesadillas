cce044d37ae7e5ba921c2a9e42d6b2621066b28f
Dav <david.gurquiza@gmail.com>
2012-03-31 14:24:06 -0600

    Added wget output format

--- hypem-downloader/hypem-magnet.js
+++ hypem-downloader/hypem-magnet.js
@@ -1,9 +1,10 @@
 $ = jQuery;
 var playbtns = $('.play-ctrl');
 var tracknames = $('.track_name');
-for(var i, l=playbtns.length;i<l<i++){
+for(var i=0, l=playbtns.length;i<l;i++){
 	playbtns.eq(i).click();
-	var trackname = tracknames.eq(i).text().replace(/\t+/g,' ').replace(/(\s\s)+/g,'').trim();
-	console.log(trackname+' : '+currentPlayerObj[0].url);
+	var trackname = tracknames.eq(i).text().replace(/\s{2,}/g,'').replace('-', ' - ');
+	//console.log(trackname+' -> '+currentPlayerObj[0].url);
+	console.log('wget -O "'+ trackname+'" "'+currentPlayerObj[0].url+'"');
 }
 
