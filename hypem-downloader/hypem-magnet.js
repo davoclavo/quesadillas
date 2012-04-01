b5bc5c42a0b8836ca007eac7c4be9488484161f7
Dav <david.gurquiza@gmail.com>
2012-03-31 18:43:57 -0600

    Added .mp3 extension and todo list

--- hypem-downloader/hypem-magnet.js
+++ hypem-downloader/hypem-magnet.js
@@ -1,3 +1,7 @@
+/*TO-DO
+> Add Download link next to the play button
+> Bookmark javascript
+*/
 $ = jQuery;
 var playbtns = $('.play-ctrl');
 var tracknames = $('.track_name');
@@ -5,6 +9,5 @@ for(var i=0, l=playbtns.length;i<l;i++){
 	playbtns.eq(i).click();
 	var trackname = tracknames.eq(i).text().replace(/\s{2,}/g,'').replace('-', ' - ');
 	//console.log(trackname+' -> '+currentPlayerObj[0].url);
-	console.log('wget -O "'+ trackname+'" "'+currentPlayerObj[0].url+'"');
-}
-
+	console.log('wget -O "'+ trackname+'.mp3" "'+currentPlayerObj[0].url+'"');
+};
