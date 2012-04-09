47d5d9d9bfc97a7d86378e6b26a9f7ab7ccc821f
Dav <david.gurquiza@gmail.com>
2012-04-08 20:55:31 -0500

    Improved the way to fetch the files urls by doing an ajax request, no need to play song by song anymore

--- hypem-downloader/hypem-downloader.js
+++ hypem-downloader/hypem-downloader.js
@@ -4,19 +4,16 @@
 */
 
 $ = jQuery;
-var playbtns = $('.play-ctrl'),
-	tracknames = $('.track_name'),
-	favdivs = $('.favdiv');
 
-for(var i=0, l=playbtns.length;i<l;i++){
-	playbtns.eq(i).click();
-	var trackname = tracknames.eq(i).text().replace(/\s{2,}/g,'').replace('-', ' - '),
-		url = currentPlayerObj[0].url;
-	//console.log(trackname+' -> '+currentPlayerObj[0].url);
-	//console.log('wget -O "'+trackname+'.mp3" "'+url+'"');
-	favdivs.eq(i).after('<li class="downdiv"><a class="down_1k9pt" href="'+url+'" download="'+trackname+'.mp3"title="Download '+trackname+'"><img height="18px" src="http://i.imgur.com/I6EYM.png"></a></li>')
-};
-
-playbtns.eq(0).click().click(); //Return player to first item
-
-//onclick="download('item','1k9pt');return false;"
+for(var i=0, l=trackList[document.location.href].length;i<l;i++){
+	$.ajax({
+		url: '/serve/source/' + trackList[activeList][i].id + '/' + trackList[activeList][i].key,
+		success: function(data){
+			var url = data.url,
+				id = data.itemid,
+				trackname = $('#section-track-'+id).find('.track_name').text().replace(/\s{2,}/g,'').replace('-', ' - ');
+			$('#section-track-'+id).find('.favdiv').after('<li class="downdiv"><a class="down_1k9pt" href="'+url+'" download="'+trackname+'.mp3"title="Download: '+trackname+'"><img height="18px" src="http://i.imgur.com/I6EYM.png"></a></li>');
+			console.log(trackname+' -> '+url);
+		}
+	});
+}
