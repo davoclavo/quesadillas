950b1d037e0476c41dd865c297736a4d6e905c43
David <david.gurquiza@gmail.com>
2012-08-31 19:50:29 -0500

    Updated code, fixed multiple download icons --- TO DO: fix force download when clicked

--- hypem-downloader/hypem-downloader.js
+++ hypem-downloader/hypem-downloader.js
@@ -2,28 +2,29 @@ setInterval(getIcons,1000)
 $ = jQuery;
 
 function getIcons(){
-	if(typeof trackList != 'undefined'){
-		if($('#loading-download').length == 0)
-			$('.no-remixes').after('<li id="loading-download"><a>Loading links... <img src="/images/ajax-loader-directory.gif"></img></a></li>').hide();
+    var trackList = displayList['tracks'];
+    if(typeof trackList != 'undefined' && !displayList['downloads']){
+        if($('#loading-download').length == 0)
+            $('.no-remixes').after('<li id="loading-download"><a>Loading links... <img src="/images/ajax-loader-directory.gif"></img></a></li>').hide();
 
-		for(var i=0, l=trackList[document.location.href].length;i<l;i++){
-			var id = trackList[document.location.href][i].id;
-			if($('#section-track-'+id).find('.downdiv').length == 0){
-				$('#loading-download').show();
-				$.ajax({
-					url: '/serve/source/' + trackList[activeList][i].id + '/' + trackList[activeList][i].key,
-					success: function(data){
-						var url = data.url,
-							id = data.itemid,
-							trackname = $('#section-track-'+id).find('.track_name').text().replace(/\s{2,}/g,'').replace('-', ' - ');
-						$('#section-track-'+id).find('.favdiv').after('<li class="downdiv"><a class="down_'+id+'" href="'+url+'" download="'+trackname+'.mp3"title="Download: '+trackname+'"><img height="18px" src="http://i.imgur.com/I6EYM.png"> </a></li>');
-						//console.log(trackname+' -> '+url);
-						$('#loading-download').hide();
-					}
-				});
-			}
-		}
-	}
+        for(var i=0, l=trackList.length;i<l;i++){
+            var id = trackList[i].id;
+            if($('#section-track-'+id).find('.downdiv').length == 0){
+                $('#loading-download').show();
+                $.ajax({
+                    url: '/serve/source/' + trackList[i].id + '/' + trackList[i].key,
+                    success: function(data){
+                        var url = data.url,
+                            id = data.itemid,
+                            trackname = $('#section-track-'+id).find('.track_name').text().replace(/\s{2,}/g,'').replace('-', ' - ');
+                        if($('#section-track-'+id).find('.downtrack').length == 0)
+                            $('#section-track-'+id).find('.favdiv').after('<a class="downtrack" id="down_'+id+'" href="'+url+'" download="'+trackname+'.mp3"title="Download: '+trackname+'"> <img height="18px" src="http://i.imgur.com/I6EYM.png"></a>');
+                        //console.log(trackname+' -> '+url);
+                        $('#loading-download').hide();
+                    }
+                });
+            }
+        }
+        displayList['downloads'] = true
+    }
 }
-
-
