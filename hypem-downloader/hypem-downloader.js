e45e669b52a67d5213f9ce025a6ca2ea04caa632
Dav <david.gurquiza@gmail.com>
2012-10-13 02:05:48 -0500

    Added jQuery closure, and the best part... made links DRAGGABLE! (Only works in Chrome btw)

--- hypem-downloader/hypem-downloader.js
+++ hypem-downloader/hypem-downloader.js
@@ -1,30 +1,34 @@
-setInterval(getIcons,1000)
-$ = jQuery;
 
-function getIcons(){
-    var trackList = displayList['tracks'];
-    if(typeof trackList != 'undefined' && !displayList['downloads']){
-        if($('#loading-download').length == 0)
-            $('.no-remixes').after('<li id="loading-download"><a>Loading links... <img src="/images/ajax-loader-directory.gif"></img></a></li>').hide();
+(function($){
+    setInterval(getIcons,1000)
+    function getIcons(){
+        var trackList = displayList['tracks']
+        if(typeof trackList != 'undefined' && !displayList['downloads']){
+            if($('#loading-download').length == 0)
+                $('<li id="loading-download"><a><img src="/images/ajax-loader-directory.gif"></img></a></li>').appendTo('#submenu').hide()
 
-        for(var i=0, l=trackList.length;i<l;i++){
-            var id = trackList[i].id;
-            if($('#section-track-'+id).find('.downdiv').length == 0){
-                $('#loading-download').show();
-                $.ajax({
-                    url: '/serve/source/' + trackList[i].id + '/' + trackList[i].key,
-                    success: function(data){
-                        var url = data.url,
-                            id = data.itemid,
-                            trackname = $('#section-track-'+id).find('.track_name').text().replace(/\s{2,}/g,'').replace('-', ' - ');
-                        if($('#section-track-'+id).find('.downtrack').length == 0)
-                            $('#section-track-'+id).find('.favdiv').after('<a class="downtrack" id="down_'+id+'" href="'+url+'" download="'+trackname+'.mp3"title="Download: '+trackname+'"> <img height="18px" src="http://i.imgur.com/I6EYM.png"></a>');
-                        //console.log(trackname+' -> '+url);
-                        $('#loading-download').hide();
-                    }
-                });
+            for(var i=0, l=trackList.length;i<l;i++){
+                var id = trackList[i].id
+                if($('#section-track-'+id).find('.downdiv').length == 0){
+                    $('#loading-download').show()
+                    $.ajax({
+                        url: '/serve/source/' + trackList[i].id + '/' + trackList[i].key,
+                        success: function(data){
+                            var url = data.url,
+                                id = data.itemid,
+                                trackname = $('#section-track-'+id+' .track_name').text().replace(/\s{2,}/g,'').replace('-', ' - ')
+                            if($('#section-track-'+id+' .downtrack').length == 0)
+                                $('#section-track-'+id+' .tools').append('<a class="dragout" id="down_'+id+'" href="'+url+'" download="'+trackname+'.mp3"title="Drag me! - Download: '+trackname+'" data-downloadurl="audio/mpeg:'+trackname+'.mp3:'+url+' draggable="true"> <img height="18px" src="http://i.imgur.com/I6EYM.png"></a>')
+                                $('#section-track-'+id+' .dragout')[0].addEventListener('dragstart', function(e) {
+                                                                        e.dataTransfer.setData('DownloadURL', this.dataset.downloadurl)
+                                                                      }, false)
+                            //console.log(trackname+' -> '+url)
+                            $('#loading-download').hide()
+                        }
+                    })
+                }
             }
+            displayList['downloads'] = true
         }
-        displayList['downloads'] = true
     }
-}
+})(jQuery)
